a949afb6c9b184db1ff2068089317f474c269c36
Meagan Chase <meaganchase814@yahoo.com>
2018-10-09 14:34:23 -0400

    it works!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
+
 class App extends Component {
+  constructor (props){
+    super(props)
+    
+    this.state = {
+      color: [
+        {backgroundColor: this.color()}, {backgroundColor: this.color()}, 
+        {backgroundColor: this.color()}, {backgroundColor: this.color()}, 
+        {backgroundColor: this.color()}, {backgroundColor: this.color()}, 
+        {backgroundColor: this.color()}, {backgroundColor: this.color()}, 
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+        {backgroundColor: this.color()}, {backgroundColor: this.color()},
+      ],
+      
+    }
+
+    this.color = this.color.bind(this)
+    this.changeColor = this.changeColor.bind(this)
+  
+  }
+  color() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+  changeColor(Id){
+    let temp = [...this.state.color] //temp = copy of array
+    temp[Id] = {backgroundColor: this.color()}
+    console.log(Id)
+    this.setState({
+      color: temp
+    });
+  }
+  drawDiv(){
+    let divArray=[]
+    // for (var i=0; i < 5; i++)
+    {[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31].map( (i) =>
+    {
+      divArray[i] = <div className="box" style={this.state.color[i]} onMouseOver={() => {this.changeColor(i)}}></div>
+        
+    })}
+    return divArray
+  }
+  
+
+    
   render() {
+   
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.drawDiv()}
       </div>
     );
   }
